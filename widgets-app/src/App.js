0833882e9eb8b61ad5340d30b7d2fb63c96ff98f
Khuziama <rehmankhuziama@gmail.com>
2021-07-06 10:54:02 -0400

    use ref hook for dropdown

--- widgets-app/src/App.js
+++ widgets-app/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
 	{
@@ -16,12 +17,27 @@ const items = [
 		content: 'React can be used by creating components'
 	}
 ];
+const options = [
+	{
+		label: 'The Color Red',
+		value: 'red'
+	},
+	{
+		label: 'The Color Blue',
+		value: 'blue'
+	},
+	{
+		label: 'The Color Green',
+		value: 'green'
+	}
+];
 
 export default () => {
+	const [ selected, setSelected ] = useState(options[0]);
 	return (
 		<div>
 			{/* <Accordion items={items} /> */}
-			<Search />
+			<Dropdown options={options} selected={selected} onSelectedChange={setSelected} />
 		</div>
 	);
 };
--- widgets-app/src/components/Search.js
+++ widgets-app/src/components/Search.js
@@ -18,14 +18,30 @@ const Search = () => {
 				});
 				setResults(data.query.search);
 			};
+			if (term && !results.length) {
+				search();
+			} else {
+				const timer = setTimeout(() => {
+					if (term) {
+						search();
+					}
+				}, 700);
 
-			search();
+				return () => {
+					clearTimeout(timer);
+				};
+			}
 		},
 		[ term ]
 	);
 	const renderedResults = results.map((result) => {
 		return (
 			<div key={result.pageid} className="item">
+				<div className="right floated content">
+					<a className="ui button" href={`https:en.wikipedia.org?curid=${result.pageid}`}>
+						Go
+					</a>
+				</div>
 				<div className="content">
 					<div className="header">{result.title}</div>
 					<span dangerouslySetInnerHTML={{ __html: result.snippet }} />
